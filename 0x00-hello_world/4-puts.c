d45c5b8860a8e2ba94e3dcf722c591d68e2209af
sogomo <tylorsoggy24@gmail.com>
2022-12-28 01:05:56 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*string intialization*/
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr); /*writing Mystr to stdout*/
+		
+	return (0);
+}
