5e7be99e037e93337baf29faecaa8b543c69a17f
sogomo <tylorsoggy24@gmail.com>
2023-01-03 19:58:56 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: Print alphabet using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char al[] = {'a', 'b', 'c', 'd', 'f',
+		'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'n', 'o', 'p', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z'};
+
+	int i = 0;
+
+	while (i < 26)
+	{
+		putchar(al[i]);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
