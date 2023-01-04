d55826d8347337e80e00e6ca9587b515ce1d914f
sogomo <tylorsoggy24@gmail.com>
2023-01-03 20:24:10 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print array of numbers 0 - 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0, i < 10, i++)
+	{
+		printf("%d",i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
